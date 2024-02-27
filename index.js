99bcd093e5f1a49a5b07028c59e26641c598d495
radyaadi <adiradya140@gmail.com>
2024-02-27 13:58:48 +0700

    add cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-// app.use(cors({ origin: '' }));
+app.use(cors());
 
 app.use('/portfolio', portfolioRoute);
 app.use('/category', categoryRoute);
